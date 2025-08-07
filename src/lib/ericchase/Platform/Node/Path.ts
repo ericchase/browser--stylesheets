e23cf550ab6a00a5fc173b29aa4d3290171be649
ericchase <github@eric.addymail.com>
2025-08-07 08:44:41 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/lib/ericchase/Platform/Node/Path.ts
+++
@@ -1,7 +0,0 @@
-import node_path from 'node:path';
-
-export function ParsePath(path: string) {
-  return node_path.parse(path);
-}
-
-export const PathSeparator = node_path.sep;
--- src/lib/ericchase/Utility/Type.ts
+++
@@ -1,3 +0,0 @@
-export type PrettyIntersection<T> = {
-  [K in keyof T]: T[K];
-};
